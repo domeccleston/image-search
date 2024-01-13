36f558d4a6c12f8c76ea09907b08284166d70976
Dom <domeccleston@gmail.com>
2024-01-13 13:58:09 +0000

    init

---
+++ main.py
@@ -0,0 +1,14 @@
+from sentence_transformers import SentenceTransformer, util
+from PIL import Image
+import glob
+import torch
+import pickle
+import zipfile
+from IPython.display import display
+from IPython.display import Image as IPImage
+import os
+from tqdm.autonotebook import tqdm
+import os
+
+data_path = 'flickr8k/Images'
+print(os.listdir(data_path))
